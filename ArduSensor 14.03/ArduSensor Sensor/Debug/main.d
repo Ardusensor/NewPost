f4ed21c225eb7c5e100fe5021c894903dfac1e76
Jaan Hendrik Murumets <Jaan Hendrik Murumets>
2016-07-20 12:18:54 +0300

    Cleaning up

---
+++ ArduSensor 14.03/ArduSensor Sensor/Debug/main.d
@@ -0,0 +1,75 @@
+main.d main.o: .././main.c \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ G:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom48p.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h \
+ .././Common.h .././uart.h
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+G:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom48p.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h:
+
+.././Common.h:
+
+.././uart.h:
--- ArduSensor 14.03/ArduSensor Sensor/main.c
+++ ArduSensor 14.03/ArduSensor Sensor/main.c
@@ -14,9 +14,9 @@ TODO:	- ADC Noise reduction
 #define _DL_ "40E53A3A"						// 40AA8840   vmcas1 - 40E53A3A  VMCAS2 - 40E568D2
 
 #define postID "6969"						// Device ID
-#define MEASURE_COUNTS 6					// How many times are we measuring capacitance? This will determine how much avaraging we will be doing.
+#define MEASURE_COUNTS 6					// How many times are we measuring capacitance? This will determine how much averaging we will be doing.
 #define measInterval 20						// Measuring interval
-#define WDOG_TIMEOUT WDTO_1S				// WD wont work without it... 
+#define WDOG_TIMEOUT WDTO_1S				        // WD wont work without it... 
 #define WDTIMEO 1000						// Watchdog timeout| OPTIONS: 250 = 0.25sec; 500 = 0.5sec; 1000 = 1sec; 2000 = 2sec; 4000 = 4sec; 8000 = 8sec;
 
 #include <stdio.h>
@@ -31,9 +31,6 @@ TODO:	- ADC Noise reduction
 #include "Common.h"
 #include "uart.h"
 
-
-
-
 // Variables
 uint16_t capValue = 0;
 uint16_t measCounter = 0;
@@ -42,93 +39,75 @@ char buffer[64];
 uint16_t sleepCounter = 0;
 
 // Function prototypes
-uint8_t confADC(void);
-uint8_t confSQG(void);
+void confADC(void);
+void confSQG(void);
 uint16_t getADC(uint8_t sensor);
-uint8_t confXBEE(void);
-
-
-
+void confXBEE(void);
 
 // This function configures ADC
-uint8_t confADC(void)
+void confADC(void)
 {
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	// Set ADC prescaler to 128x [125kHz @ 16MHz]
-	SETBIT(ADMUX,REFS0);									// Set reference voltage to AVcc
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	        // Set ADC prescaler to 128x [125kHz @ 16MHz]
+	SETBIT(ADMUX,REFS0);						// Set reference voltage to AVcc
 	
 	CLEARBIT(ADCSRA, ADATE);
 	
-	ADCSRA |= (1 << ADEN) | (1 << ADSC);	// Enable ADATE, ADC and start A2D conversions
-	return 1;
+	ADCSRA |= (1 << ADEN) | (1 << ADSC);	                        // Enable ADATE, ADC and start A2D conversions
 }
 
 
 // This function configures Square Wave Generator
-uint8_t confSQG(void) 
+void confSQG(void)
 {
 	// Set up timer: 
-	SETBIT(DDRD,PD6);				// Set PD6[Square Wave Output] as output.
-	SETBIT(DDRD,PD5);				// Set PD5[Square Wave Output] as output.
+	SETBIT(DDRD,PD6);				                // Set PD6[Square Wave Output] as output.
+	SETBIT(DDRD,PD5);				                // Set PD5[Square Wave Output] as output.
 	
 	TCCR0A = 0b00110011;
 	TCCR0B = 0b00001001;
 	
 	OCR0A = 15;
-	OCR0B = 8;			// Set compare match value. 11 is 25% 
-	TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));		// Disables SQG output
-	return 1;
-	
-	//TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));		// Disables SQG output
-	//TCCR0A |= (1 << COM0B1) | (1 << COM0B0);		// Enables SQG output
+	OCR0B = 8;			                                // Set compare match value. 11 is 25% 
+	TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));		        // Disables SQG output
 }
 
 // This function configures xBee
-uint8_t confXBEE(void) {
-		/* uart_puts("POST ID:");
-		 uart_puts("\r");			// Set address
-		 _delay_ms(10);
-		 uart_puts(postID);
-		 uart_puts("\r");
-		 _delay_ms(10);*/
-		 
-		 uart_puts("+++"); 			//Enter command mode
-		 _delay_ms(2000);
+void confXBEE(void)
+{
+	uart_puts("+++"); 			                        // Enter command mode
+	_delay_ms(2000);
 
-		 uart_puts("ATID 4000\r"); 	// Set PAN ID //1342  VMCAS1 ID - 4000; VMCAS2 ID - 4001
-		 _delay_ms(500);
+	uart_puts("ATID 4000\r"); 	                                // Set PAN ID //1342  VMCAS1 ID - 4000; VMCAS2 ID - 4001
+	_delay_ms(500);
 		 
-		 uart_puts("ATDH"); 			// Set address
-		 _delay_ms(10);
-		 uart_puts(_DH_); 			// Written in #Define
-		 uart_puts("\r");
-		 _delay_ms(100);
+	uart_puts("ATDH"); 			                        // Set address
+	_delay_ms(10);
+	uart_puts(_DH_); 			                        // Written in #define
+	uart_puts("\r");
+	_delay_ms(100);
 		 
-		 uart_puts("ATDL");
-		 _delay_ms(10);
-		 uart_puts(_DL_);			// Written in #Define
-		 uart_puts("\r");
-		 _delay_ms(100);
+	uart_puts("ATDL");
+	_delay_ms(10);
+	uart_puts(_DL_);			                        // Written in #define
+	uart_puts("\r");
+	_delay_ms(100);
 		 
+	uart_puts("ATNH 1E\r");		                                // Required XBee constants
+	_delay_ms(100);
+	uart_puts("ATNO 3\r");
+	_delay_ms(100);
+	uart_puts("ATSP 7D0\r");
+	_delay_ms(100);
+	uart_puts("ATSN 21C\r");
+	_delay_ms(100);
+	uart_puts("ATSM 1\r");
+	_delay_ms(100);
 		 
-		 uart_puts("ATNH 1E\r");		// Required XBee constants
-		 _delay_ms(100);
-		 uart_puts("ATNO 3\r");
-		 _delay_ms(100);
-		 uart_puts("ATSP 7D0\r");
-		 _delay_ms(100);
-		 uart_puts("ATSN 21C\r");
-		 _delay_ms(100);
-		 uart_puts("ATSM 1\r");
-		 _delay_ms(100);
+	uart_puts("ATWR\r");		                                // Save configuration
+	_delay_ms(2000);
 		 
-		 uart_puts("ATWR\r");		// Save configuration
-		 _delay_ms(2000);
-		 
-		 uart_puts("ATCN\r");		// Exit Command mode
-		 _delay_ms(1000);
-		 
-		 return 1;
-	
+	uart_puts("ATCN\r");		                                // Exit Command mode
+	_delay_ms(1000);
 }
 
 // Function for getting different ADC values
@@ -136,66 +115,52 @@ uint16_t getADC(uint8_t sensor)
 {
 	uint8_t i;
 	capValue = 0;
-	CLEARBIT(PORTD, PD2);								// Turn on peripheral mosfet!
-	SETBIT(ADCSRA, ADEN);								// Enable ADC module
-	SETBIT(ADCSRA, ADSC);								// Start first conversion
+	CLEARBIT(PORTD, PD2);						// Turn on peripheral mosfet!
+	SETBIT(ADCSRA, ADEN);						// Enable ADC module
+	SETBIT(ADCSRA, ADSC);						// Start first conversion
 	_delay_ms(1);
+	
 	switch(sensor) {
-		
-		// Main sensor measuring
-		case 0:
-		TCCR0A |= (1 << COM0B1) | (1 << COM0B0);			// Enables SQG output
-		
-		ADMUX &= 0b11110000;								// Set ADC channel to ADC0
+	case 0: // Main sensor measuring
+		TCCR0A |= (1 << COM0B1) | (1 << COM0B0);		// Enables SQG output
+		ADMUX &= 0b11110000;				        // Set ADC channel to ADC0
 		_delay_ms(1);
 		break;
-		
-		// Calibration sensor measuring
-		case 1:
-		TCCR0A |= (1 << COM0B1) | (1 << COM0B0);			// Enables SQG output
-		
+	case 1: // Calibration sensor measuring
+		TCCR0A |= (1 << COM0B1) | (1 << COM0B0);	        // Enables SQG output
 		ADMUX &= 0b11110000;
-		ADMUX |= 0b00000010;								// Set ADC channel to ADC2
+		ADMUX |= 0b00000010;					// Set ADC channel to ADC2
 		_delay_ms(1);
-		break;
-		
-		// Voltage measuring
-		case 2:
+		break;	
+	case 2: // Voltage measuring
 		ADMUX &= 0b11110000;
 		ADMUX |= 0b00000001;
-		break;
-		
-		// External temperature IC measuring
-		case 3:
+		break;	
+	case 3: // External temperature IC measuring
 		ADMUX &= 0b11110000;
 		ADMUX |= 0b00000011;
-		break;
-		
-		// Internal CPU temperature measuring
-		case 4:
+		break;	
+	case 4: // Internal CPU temperature measuring
 		ADMUX &= 0b11110000;
 		ADMUX |= 0b00001000;
 		break;
 	}
 	
 	for (i = 0; i < MEASURE_COUNTS; i++){
-		SETBIT(ADCSRA, ADSC);							// Start the ADC conversion
+		SETBIT(ADCSRA, ADSC);					// Start the ADC conversion
 		_delay_ms(2);
 		measurements[i] = 0;
 		measurements[i] = ADC;
 		capValue += measurements[i];
 	}
 	
-	capValue = capValue/i;
+	capValue = capValue / i;
 	
-	CLEARBIT(ADCSRA, ADEN);								// Disable ADC
+	CLEARBIT(ADCSRA, ADEN);						// Disable ADC
 	TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));			// Disables SQG output
-	SETBIT(PORTD, PD2);									// Turn off peripheral mosfet
+	SETBIT(PORTD, PD2);						// Turn off peripheral mosfet
 	
 	return capValue;
-	
-	
-	
 }
 
 ISR(WDT_vect)
@@ -207,67 +172,58 @@ ISR(WDT_vect)
 int main(void)
 {
 	// SETUP
-	cli();												// Disable global interrupts when configuring!
-	MCUSR = 0;											// Zero all previous reset info
+	cli();								// Disable global interrupts when configuring!
+	MCUSR = 0;							// Zero all previous reset info
 	confSQG();
 	confADC();
-	SETBIT(DDRD, PD2);									// Peripheral mosfet as output
-	SETBIT(PORTD, PD2);									// Turn off that mosfet!
-	SETBIT(DDRC,PC5);									// Set xBee sleep pin as output
-	DIDR1=0xFF;											// Disable digital input pins just in case
-	wdt_disable();										// Disable Watchdog for now
-	sei();												// Enable global interrupts
+	SETBIT(DDRD, PD2);						// Peripheral mosfet as output
+	SETBIT(PORTD, PD2);						// Turn off that mosfet!
+	SETBIT(DDRC,PC5);						// Set xBee sleep pin as output
+	DIDR1 = 0xFF;							// Disable digital input pins just in case
+	wdt_disable();							// Disable Watchdog for now
+	sei();								// Enable global interrupts
 	
 	uart_init(UART_BAUD_SELECT(9600,F_CPU));			// Initialize UART at 9600 baudrate.
-	_delay_ms(5);										// Let's wait lil bit just in case
-	confXBEE();											// We will configure xBee now
+	_delay_ms(5);							// Let's wait lil bit just in case
+	confXBEE();							// We will configure xBee now
 	_delay_ms(2000);	
-	uart_puts("HELLO, WELCOME TO ARDUSENSOR! \n");		// Let's be polite and greet our guest!
+	uart_puts("HELLO, WELCOME TO ARDUSENSOR! \n");		        // Let's be polite and greet our guest!
 	uart_puts("ArduSensor is configured! \n");			// Let's also let our guest know that we are configured and good to go!
 	_delay_ms(2000);	
-	SETBIT(PORTC, PC5);									// Set xBee to sleep!
+	SETBIT(PORTC, PC5);						// Set xBee to sleep!
 	
-	
-	// Main loop
-	while(1)
-	{
-		if(sleepCounter >= measInterval){
-			
-			wdt_disable();								// Disable Watchdog
-			CLEARBIT(PORTC, PC5);						// Wake da xBee up, yo!
-			_delay_ms(4);								// Let's actually wait for xBee to wake up
-			sleepCounter = 0;							// Reset the sleep counter
+	while(1) { // Main loop
+                if (sleepCounter >= measInterval) {
+			wdt_disable();					// Disable Watchdog
+			CLEARBIT(PORTC, PC5);				// Wake da xBee up, yo!
+			_delay_ms(4);					// Let's actually wait for xBee to wake up
+			sleepCounter = 0;				// Reset the sleep counter
 			
 			uint16_t mainCap, calibCap, inTemp,
-			exTemp, batVolt = 0;						// Initialize and null the previous main cap read
+			exTemp, batVolt = 0;				// Initialize and null the previous main cap read
 			
-			mainCap = getADC(0);						// Get the main cap reading
-			calibCap = getADC(1);						// Get the calibration cap reading
+			mainCap = getADC(0);				// Get the main cap reading
+			calibCap = getADC(1);				// Get the calibration cap reading
 			batVolt = getADC(2);
 			exTemp = getADC(3);
 			inTemp = getADC(4);
-		
-		
-			/*sprintf(buffer,"<%u;%u;%u;%u;%u;%u;%s> \n",	// Formatting data for UART!  THIS IS OLD!!!
-			inTemp, exTemp, batVolt, calibCap,	
-			mainCap, measCounter++, postID);			*/ 
 			
-			sprintf(buffer,"<%u;%u;%u;%u;%u>",	// Formatting data for UART!  THIS IS OLD!!!
+			sprintf(buffer,"<%u;%u;%u;%u;%u>",	        // Formatting data for UART!  THIS IS OLD!!!
 			exTemp, batVolt, calibCap,
 			mainCap, measCounter++);
 					
-			uart_puts(buffer);							// Sending to UART/xBee
-			_delay_ms(40);								// Lil bit of delay cuz xBee is needy mo'fucka
+			uart_puts(buffer);				// Sending to UART/xBee
+			_delay_ms(40);					// Lil bit of delay cuz xBee is needy
 			
-			SETBIT(PORTC, PC5);							// Let xBee sleep, nigga!
+			SETBIT(PORTC, PC5);				// Let XBee sleep
 		}
 		
-		sleepCounter++;									// We will increment the sleepcounter now!
+		sleepCounter++;						// We will increment the sleepcounter now!
 		
 		// Configuring WatchDog
 		cli();
 		WDTCSR |= (1 << WDCE);
-		wdt_enable(WDOG_TIMEOUT);						// Pointless basically but WD does not work without it. 
+		wdt_enable(WDOG_TIMEOUT);				// Pointless basically but WD does not work without it. 
 		WDTCSR |= (1 << WDCE);
 		WDTCSR = __WDCONF__;
 		wdt_reset();
@@ -279,10 +235,5 @@ int main(void)
 		sleep_bod_disable();
 		sleep_cpu();
 		sleep_disable();
-		
 	}
-	
 }
-
-
-
