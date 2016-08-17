af4e9dd59a3c2209b284ea440cc0a59280085d80
Jaan Hendrik Murumets <Jaan Hendrik Murumets>
2016-08-17 15:49:05 +0300

    Cleaning

--- ArduSensor 14.03/ArduSensor Sensor/main.c
+++ ArduSensor 14.03/ArduSensor Sensor/main.c
@@ -4,20 +4,20 @@
  * Created: 2016-03-15
  * Author: Kristjan Tozen
  * Based on Cmc code
- *
+ 
 TODO:	- ADC Noise reduction
-		- WDT work completely
+	- WDT work completely
  */ 
 
-#define F_CPU 16000000UL					// UART needs this
-#define _DH_ "0013A200" 					// XBee address of the destination Supernode //0013A200
-#define _DL_ "40E53A3A"						// 40AA8840   vmcas1 - 40E53A3A  VMCAS2 - 40E568D2
+#define F_CPU 16000000UL					        // UART needs this
+#define _DH_ "0013A200" 					        // XBee address of the destination Supernode //0013A200
+#define _DL_ "40E53A3A"						        // 40AA8840   vmcas1 - 40E53A3A  VMCAS2 - 40E568D2
 
-#define postID "6969"						// Device ID
-#define MEASURE_COUNTS 6					// How many times are we measuring capacitance? This will determine how much averaging we will be doing.
-#define measInterval 20						// Measuring interval
-#define WDOG_TIMEOUT WDTO_1S				        // WD wont work without it... 
-#define WDTIMEO 1000						// Watchdog timeout| OPTIONS: 250 = 0.25sec; 500 = 0.5sec; 1000 = 1sec; 2000 = 2sec; 4000 = 4sec; 8000 = 8sec;
+#define postID "6969"					        	// Device ID
+#define MEASURE_COUNTS 6				        	// How many times are we measuring capacitance? This will determine how much averaging we will be doing.
+#define measInterval 20						        // Measuring interval
+#define WDOG_TIMEOUT WDTO_1S				                // WD wont work without it... 
+#define WDTIMEO 1000						        // Watchdog timeout| OPTIONS: 250 = 0.25sec; 500 = 0.5sec; 1000 = 1sec; 2000 = 2sec; 4000 = 4sec; 8000 = 8sec;
 
 #include <stdio.h>
 #include <avr/io.h>
@@ -43,130 +43,6 @@ void confADC(void);
 void confSQG(void);
 uint16_t getADC(uint8_t sensor);
 void confXBEE(void);
-
-// This function configures ADC
-void confADC(void)
-{
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	        // Set ADC prescaler to 128x [125kHz @ 16MHz]
-	SETBIT(ADMUX,REFS0);						// Set reference voltage to AVcc
-	
-	CLEARBIT(ADCSRA, ADATE);
-	
-	ADCSRA |= (1 << ADEN) | (1 << ADSC);	                        // Enable ADATE, ADC and start A2D conversions
-}
-
-
-// This function configures Square Wave Generator
-void confSQG(void)
-{
-	// Set up timer: 
-	SETBIT(DDRD,PD6);				                // Set PD6[Square Wave Output] as output.
-	SETBIT(DDRD,PD5);				                // Set PD5[Square Wave Output] as output.
-	
-	TCCR0A = 0b00110011;
-	TCCR0B = 0b00001001;
-	
-	OCR0A = 15;
-	OCR0B = 8;			                                // Set compare match value. 11 is 25% 
-	TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));		        // Disables SQG output
-}
-
-// This function configures xBee
-void confXBEE(void)
-{
-	uart_puts("+++"); 			                        // Enter command mode
-	_delay_ms(2000);
-
-	uart_puts("ATID 4000\r"); 	                                // Set PAN ID //1342  VMCAS1 ID - 4000; VMCAS2 ID - 4001
-	_delay_ms(500);
-		 
-	uart_puts("ATDH"); 			                        // Set address
-	_delay_ms(10);
-	uart_puts(_DH_); 			                        // Written in #define
-	uart_puts("\r");
-	_delay_ms(100);
-		 
-	uart_puts("ATDL");
-	_delay_ms(10);
-	uart_puts(_DL_);			                        // Written in #define
-	uart_puts("\r");
-	_delay_ms(100);
-		 
-	uart_puts("ATNH 1E\r");		                                // Required XBee constants
-	_delay_ms(100);
-	uart_puts("ATNO 3\r");
-	_delay_ms(100);
-	uart_puts("ATSP 7D0\r");
-	_delay_ms(100);
-	uart_puts("ATSN 21C\r");
-	_delay_ms(100);
-	uart_puts("ATSM 1\r");
-	_delay_ms(100);
-		 
-	uart_puts("ATWR\r");		                                // Save configuration
-	_delay_ms(2000);
-		 
-	uart_puts("ATCN\r");		                                // Exit Command mode
-	_delay_ms(1000);
-}
-
-// Function for getting different ADC values
-uint16_t getADC(uint8_t sensor)
-{
-	uint8_t i;
-	capValue = 0;
-	CLEARBIT(PORTD, PD2);						// Turn on peripheral mosfet!
-	SETBIT(ADCSRA, ADEN);						// Enable ADC module
-	SETBIT(ADCSRA, ADSC);						// Start first conversion
-	_delay_ms(1);
-	
-	switch(sensor) {
-	case 0: // Main sensor measuring
-		TCCR0A |= (1 << COM0B1) | (1 << COM0B0);		// Enables SQG output
-		ADMUX &= 0b11110000;				        // Set ADC channel to ADC0
-		_delay_ms(1);
-		break;
-	case 1: // Calibration sensor measuring
-		TCCR0A |= (1 << COM0B1) | (1 << COM0B0);	        // Enables SQG output
-		ADMUX &= 0b11110000;
-		ADMUX |= 0b00000010;					// Set ADC channel to ADC2
-		_delay_ms(1);
-		break;	
-	case 2: // Voltage measuring
-		ADMUX &= 0b11110000;
-		ADMUX |= 0b00000001;
-		break;	
-	case 3: // External temperature IC measuring
-		ADMUX &= 0b11110000;
-		ADMUX |= 0b00000011;
-		break;	
-	case 4: // Internal CPU temperature measuring
-		ADMUX &= 0b11110000;
-		ADMUX |= 0b00001000;
-		break;
-	}
-	
-	for (i = 0; i < MEASURE_COUNTS; i++){
-		SETBIT(ADCSRA, ADSC);					// Start the ADC conversion
-		_delay_ms(2);
-		measurements[i] = 0;
-		measurements[i] = ADC;
-		capValue += measurements[i];
-	}
-	
-	capValue = capValue / i;
-	
-	CLEARBIT(ADCSRA, ADEN);						// Disable ADC
-	TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));			// Disables SQG output
-	SETBIT(PORTD, PD2);						// Turn off peripheral mosfet
-	
-	return capValue;
-}
-
-ISR(WDT_vect)
-{
-	// Do nothing, we just woke up the monster!
-}
 
 // THIS IS OUR MAIN FUNCTION
 int main(void)
@@ -199,14 +75,11 @@ int main(void)
 			_delay_ms(4);					// Let's actually wait for xBee to wake up
 			sleepCounter = 0;				// Reset the sleep counter
 			
-			uint16_t mainCap, calibCap, inTemp,
-			exTemp, batVolt = 0;				// Initialize and null the previous main cap read
-			
-			mainCap = getADC(0);				// Get the main cap reading
-			calibCap = getADC(1);				// Get the calibration cap reading
-			batVolt = getADC(2);
-			exTemp = getADC(3);
-			inTemp = getADC(4);
+			uint16_t mainCap = getADC(0);                   // Get the main cap reading
+                        uint16_t calibCap = getADC(1);                  // Get the calibration cap reading
+                        uint16_t batVolt = getADC(2);
+			uint16_t exTemp = getADC(3);
+                        uint16_t inTemp = getADC(4);
 			
 			sprintf(buffer,"<%u;%u;%u;%u;%u>",	        // Formatting data for UART!  THIS IS OLD!!!
 			exTemp, batVolt, calibCap,
@@ -218,7 +91,7 @@ int main(void)
 			SETBIT(PORTC, PC5);				// Let XBee sleep
 		}
 		
-		sleepCounter++;						// We will increment the sleepcounter now!
+		sleepCounter++;						// We will increment the sleep counter now!
 		
 		// Configuring WatchDog
 		cli();
@@ -236,4 +109,127 @@ int main(void)
 		sleep_cpu();
 		sleep_disable();
 	}
+}
+
+// This function configures ADC
+void confADC(void)
+{
+        ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	        // Set ADC prescaler to 128x [125kHz @ 16MHz]
+        SETBIT(ADMUX,REFS0);						// Set reference voltage to AVcc
+        
+        CLEARBIT(ADCSRA, ADATE);
+        
+        ADCSRA |= (1 << ADEN) | (1 << ADSC);	                        // Enable ADATE, ADC and start A2D conversions
+}
+
+
+// This function configures Square Wave Generator
+void confSQG(void)
+{
+        // Set up timer:
+        SETBIT(DDRD,PD6);				                // Set PD6[Square Wave Output] as output.
+        SETBIT(DDRD,PD5);				                // Set PD5[Square Wave Output] as output.
+        
+        TCCR0A = 0b00110011;
+        TCCR0B = 0b00001001;
+        
+        OCR0A = 15;
+        OCR0B = 8;			                                // Set compare match value. 11 is 25%
+        TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));		        // Disables SQG output
+}
+
+// This function configures xBee
+void confXBEE(void)
+{
+        uart_puts("+++"); 			                        // Enter command mode
+        _delay_ms(2000);
+
+        uart_puts("ATID 4000\r"); 	                                // Set PAN ID //1342  VMCAS1 ID - 4000; VMCAS2 ID - 4001
+        _delay_ms(500);
+        
+        uart_puts("ATDH"); 			                        // Set address
+        _delay_ms(10);
+        uart_puts(_DH_); 			                        // Written in #define
+        uart_puts("\r");
+        _delay_ms(100);
+        
+        uart_puts("ATDL");
+        _delay_ms(10);
+        uart_puts(_DL_);			                        // Written in #define
+        uart_puts("\r");
+        _delay_ms(100);
+        
+        uart_puts("ATNH 1E\r");		                                // Required XBee constants
+        _delay_ms(100);
+        uart_puts("ATNO 3\r");
+        _delay_ms(100);
+        uart_puts("ATSP 7D0\r");
+        _delay_ms(100);
+        uart_puts("ATSN 21C\r");
+        _delay_ms(100);
+        uart_puts("ATSM 1\r");
+        _delay_ms(100);
+        
+        uart_puts("ATWR\r");		                                // Save configuration
+        _delay_ms(2000);
+        
+        uart_puts("ATCN\r");		                                // Exit Command mode
+        _delay_ms(1000);
+}
+
+// Function for getting different ADC values
+uint16_t getADC(uint8_t sensor)
+{
+        capValue = 0;
+        CLEARBIT(PORTD, PD2);						// Turn on peripheral mosfet!
+        SETBIT(ADCSRA, ADEN);						// Enable ADC module
+        SETBIT(ADCSRA, ADSC);						// Start first conversion
+        while(!BITVAL(ADCSRA, ADIF));                                   // Wait for conversion done
+        
+        switch(sensor) {
+                case 0: // Main sensor measuring
+                TCCR0A |= (1 << COM0B1) | (1 << COM0B0);		// Enables SQG output
+                ADMUX &= 0b11110000;				        // Set ADC channel to ADC0
+                _delay_ms(1);
+                break;
+                case 1: // Calibration sensor measuring
+                TCCR0A |= (1 << COM0B1) | (1 << COM0B0);	        // Enables SQG output
+                ADMUX &= 0b11110000;
+                ADMUX |= 0b00000010;					// Set ADC channel to ADC2
+                _delay_ms(1);
+                break;
+                case 2: // Voltage measuring
+                ADMUX &= 0b11110000;
+                ADMUX |= 0b00000001;
+                break;
+                case 3: // External temperature IC measuring
+                ADMUX &= 0b11110000;
+                ADMUX |= 0b00000011;
+                break;
+                case 4: // Internal CPU temperature measuring
+                ADMUX &= 0b11110000;
+                ADMUX |= 0b00001000;
+                break;
+        }
+        
+        uint8_t i;
+        for (i = 0; i < MEASURE_COUNTS; i++) {
+                SETBIT(ADCSRA, ADSC);					// Start the ADC conversion
+                while(!BITVAL(ADCSRA, ADIF));                           // Wait for conversion done
+                measurements[i] = ADC;
+                capValue += measurements[i];
+        }
+        
+        capValue /= MEASURE_COUNTS;
+        
+        CLEARBIT(ADCSRA, ADEN);						// Disable ADC
+        TCCR0A &= ~((1 << COM0B1) | (1 << COM0B0));			// Disables SQG output
+        SETBIT(PORTD, PD2);						// Turn off peripheral mosfet
+        
+        return capValue;
+}
+
+ISR(WDT_vect)
+{
+        // Do nothing, we just woke up the monster!
 }
